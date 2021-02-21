bdc82288fcd12888f904049c902c047794dc59ed
dddd <rillakerika@gmail.com>
2021-02-22 08:16:41 +0900

    sassファイルを読み込める＆ポスターを表示（途中）

--- src/components/Row.tsx
+++ src/components/Row.tsx
@@ -1,6 +1,9 @@
 // import axios from 'axios'
 import React, { useState, useEffect } from 'react'
 import API from '../axios'
+import './Row.scss'
+
+const BASE_URL = 'https://image.tmdb.org/t/p/original'
 
 interface Props {
   title: string
@@ -18,9 +21,9 @@ interface Movie {
 }
 
 export const Row: React.FunctionComponent<Props> = ({
-  // eslint-disable-next-line
   title,
   fetchUrl,
+  isLargeRow,
 }: Props) => {
   const [movies, setMovies] = useState<Movie[]>([])
 
@@ -33,5 +36,24 @@ export const Row: React.FunctionComponent<Props> = ({
   }, [fetchUrl])
 
   console.log(movies)
-  return <div className="Row" />
+  return (
+    <div className="Row">
+      <h2>{title}</h2>
+      <div className="Row-posters">
+        {
+          // eslint-disable-next-line
+          movies.map((movie, i) => (
+            <img
+              key={movie.id}
+              className={`Row-poster ${isLargeRow && 'Row-poster-large'}`}
+              src={`${BASE_URL}${
+                isLargeRow ? movie.poster_path : movie.backdrop_path
+              }`}
+              alt={movie.name}
+            />
+          ))
+        }
+      </div>
+    </div>
+  )
 }
